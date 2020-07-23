4e3909d2bf366211390c3197b98571c6d308c5f5
bbaktaeho <lth9036@gmail.com>
2020-07-23 17:48:54 +0900

    Update: stack ssequence

--- codingTest/kotlin/algorithm/src/one/스택 수열.kt
+++ codingTest/kotlin/algorithm/src/one/스택 수열.kt
@@ -5,5 +5,23 @@ fun main() {
 }
 
 fun stackSequence(N: Int) {
- // test
+    val stack = mutableListOf<Int>()
+    val result = mutableListOf<Char>()
+    var num: Int
+    var count = 1
+    for (i in 1..N) {
+        num = readLine()!!.toInt()
+        while (count <= num) {
+            stack.add(count++)
+            result.add('+')
+        }
+        if (stack.last() == num) {
+            stack.removeAt(stack.lastIndex)
+            result.add('-')
+        }else {
+            println("NO")
+            return
+        }
+    }
+    result.forEach { println(it) }
 }
