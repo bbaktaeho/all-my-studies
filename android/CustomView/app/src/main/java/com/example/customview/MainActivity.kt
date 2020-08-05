21547aa12490a59f59962f584b87069d16109152
bbaktaeho <lth9036@gmail.com>
2020-08-06 00:25:16 +0900

    Update: CustomView for CustomText

--- android/CustomView/app/src/main/java/com/example/customview/MainActivity.kt
+++ android/CustomView/app/src/main/java/com/example/customview/MainActivity.kt
@@ -14,9 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val customView: CustomView = CustomView("hello", this)
-        frameLayout.addView(customView)
     }
 }
 
