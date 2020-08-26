7d83ae048290b3df28f7e2b2ef6b54e6808dfdc0
bbaktaeho <lth9036@gmail.com>
2020-08-26 20:34:02 +0900

    Update: 조건부 타입

--- typescript/우아한tech/conditionalType.ts
+++ typescript/우아한tech/conditionalType.ts
@@ -110,3 +110,25 @@ lookupEntity(plus1(10));
 function lookupEntity(id: Id) {
   // query DB for entity by ID
 }
+
+// Function 타입 찾기
+type FunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? K : never;
+}[keyof T];
+type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
+
+type NonFunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K;
+}[keyof T];
+type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
+
+interface Person {
+  id: number;
+  name: string;
+  hello(message: string): void;
+}
+
+type T1 = FunctionPropertyNames<Person>;
+type T2 = NonFunctionPropertyNames<Person>;
+type T3 = FunctionProperties<Person>;
+type T4 = NonFunctionProperties<Person>;
