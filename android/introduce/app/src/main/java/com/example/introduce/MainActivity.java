fab643c1e151bbf052953408a1f5038834814067
bbaktaeho <lth9036@gmail.com>
2020-09-05 16:47:01 +0900

    Add: Introduce Myself

---
+++ android/introduce/app/src/main/java/com/example/introduce/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.introduce;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+    }
+}
