3f3fa85b11eac35b9701cd6a011e915f48efcd82
bbaktaeho <lth9036@gmail.com>
2020-09-05 17:00:15 +0900

    Update: TableLayout

--- android/introduce/app/src/main/java/com/example/introduce/MainActivity.java
+++ android/introduce/app/src/main/java/com/example/introduce/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.hide();
+
+        ImageView profile = findViewById(R.id.img_profile);
+        profile.setBackground(new ShapeDrawable(new OvalShape()));
+        profile.setClipToOutline(true);
     }
 }
