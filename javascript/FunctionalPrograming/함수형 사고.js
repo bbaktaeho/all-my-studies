96b4dbaa1570fa5e8682fb0530bc4303104d8ce9
bbaktaeho <lth9036@gmail.com>
2020-09-06 12:04:43 +0900

    Update: Custom Map, Filter, Reduce

--- javascript/FunctionalPrograming/함수형 사고.js
+++ javascript/FunctionalPrograming/함수형 사고.js
@@ -1,11 +1,41 @@
-// const mapF = require("./map 만들기");
-// const filter = require("./filter 만들기");
-// const reduce = require("./reduce 만들기");
-// const products = [
-//   { name: "반팔1", price: 15000 },
-//   { name: "반팔2", price: 20000 },
-//   { name: "반팔3", price: 3000 },
-//   { name: "반팔4", price: 40000 },
-// ];
+const products = [
+  { name: "반팔1", price: 15000 },
+  { name: "반팔2", price: 20000 },
+  { name: "반팔3", price: 3000 },
+  { name: "반팔4", price: 40000 },
+];
 
-// // console.log(mapF((a) => a + a, [1, 2, 3, 4, 5]));
+const map = (func, iter) => {
+  let res = [];
+  for (item of iter) res.push(func(item));
+  return res;
+};
+
+const filter = (func, iter) => {
+  let res = [];
+  for (item of iter) {
+    if (func(item)) res.push(item);
+  }
+  return res;
+};
+
+const reduce = (func, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (item of iter) acc = func(acc, item);
+  return acc;
+};
+
+// 가격 뽑기
+console.log(map((p) => p.price, products));
+
+// 특정 이하의 상품 뽑기
+console.log(map((p) => p, filter((p) => p.price < 20000, products)));
+
+// 특정 이하의 상품을 뽑아 가격 더하기
+console.log(reduce(
+  (a, b) => a + b,
+  map((p) => p.price, filter((p) => p.price < 20000, products)),
+));
