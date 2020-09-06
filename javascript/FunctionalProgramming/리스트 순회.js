b7e2eadcd0357f6b64f53b672e3d2fbf4e50fde5
bbaktaeho <lth9036@gmail.com>
2020-09-06 14:24:11 +0900

    Update: Iterator

--- javascript/FunctionalProgramming/리스트 순회.js
+++ javascript/FunctionalProgramming/리스트 순회.js
@@ -1,5 +1,12 @@
-const list = [1, 2, 3];
-const str = "문자열";
+const list = [1, 2, 3, [4, 5, 6], { key: "ef25gdf", value: "val" }];
+const str = "문자열 입니다.";
+const set = new Set([4, 5, 6, 6]);
+const map = new Map([
+  ["key1", 7],
+  ["key2", 7],
+  ["key3", 8],
+  ["key4", 9],
+]);
 
 // es5
 for (let i = 0; i < list.length; i++) {
@@ -16,26 +23,16 @@ for (const i of list) {
 for (const i of str) {
   console.log(i);
 }
-const obj = {};
 
-const set = new Set([4, 5, 6, 6]);
 for (let s of set) console.log(s);
 
-const map = new Map([
-  ["key1", 7],
-  ["key2", 7],
-  ["key3", 8],
-  ["key4", 9],
-]);
-for (let m of map) console.log(m);
+for (let m of map) console.log(m); // entries
 for (let m of map.keys()) console.log(m);
 for (let m of map.values()) console.log(m);
 for (let m of map.entries()) console.log(m);
 
-// Symbol
-// Symbol.iterator : 객체의 키로 사용 가능
 /**
- * @description 이터러블과 이터레이터 프로토콩
+ * @description 이터러블과 이터레이터 프로토콜
  * @이터러블 이터레이터를 리턴하는 [Symbol.iterator()] 를 가진 값
  * @이터레이터 {value, done} 객체는 리턴하는 next() 를 가진 값, done이 트루일 경우 빠져나옴 -> iterator.next()
  * @이터러블 @이터레이터 @프로토콜 이터러블은 for of, 전개 연산자 등과 함께 동작하도록한 규약
@@ -44,6 +41,7 @@ const itExList = [1, 2, 3];
 let itr = itExList[Symbol.iterator]();
 console.log(itr.next());
 
+console.clear();
 // ! 사용자 정의 이터러블
 const iterable = {
   [Symbol.iterator]() {
@@ -64,6 +62,3 @@ console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
-
-const arr2 = [1, 2, 3];
-const arr2Itr = arr2[Symbol.iterator]();
