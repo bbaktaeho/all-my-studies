359e93ec30afd4243dc5452c39804b1509c00345
bbaktaeho <lth9036@gmail.com>
2020-09-06 15:05:32 +0900

    Update: Custom Prototype.map

--- javascript/FunctionalProgramming/map 만들기.js
+++ javascript/FunctionalProgramming/map 만들기.js
@@ -7,33 +7,29 @@ const products = [
 
 // 순수 함수
 // 함수형 프로그래밍 과정
-module.exports = (func, iter) => {
+const map = (func, iter) => {
   let res = [];
   for (p of iter) res.push(func(p));
   return res;
 };
 
-console.log(mapF((p) => p.name, products));
-console.log(mapF((p) => p.price, products));
+console.log(map((p) => p.name, products));
+console.log(map((p) => p.price, products));
 
 // 이터러블 프로토콜을 따른 map의 다형성
 // console.log(document.querySelectorAll('*').map()); // 배열 상속을 받지 않음, 이터러블은 맞음
-
-// 제너레이터에서 순수함수 mapF 적용
+// 제너레이터에서 순수함수 map 적용
 function* gen() {
   yield 1;
   yield 2;
   yield 3;
 }
 console.log(
-  mapF((a) => a * a),
-  gen()
+  map((a) => a * a, gen()),
 );
 
 let m = new Map();
 m.set("a", 10); // -> ["a", 10]
 m.set("b", 20);
 const it = m[Symbol.iterator]();
-console.log(new MapF(mapF(([k, v]) => [k, v * 2], m)));
-
-mapF(([k, v]) => [k, v * 2], m);
+console.log(new Map(map(([k, v]) => [k, v * 2], m)));
