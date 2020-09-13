a0c554dfd5a761e646d094db57c658041d2c4f0a
bbaktaeho <lth9036@gmail.com>
2020-09-13 20:02:55 +0900

    Update: Calculator

--- android/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ android/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button btnOne;
@@ -21,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnZero;
     TextView txtExpression;
     TextView txtResult;
+    List<String> expressionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +37,10 @@ public class MainActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.hide();
 
-        init();
+        this.init();
     }
 
+    // 컴포넌트 초기화
     void init() {
         btnOne = findViewById(R.id.btn_one);
         btnTwo = findViewById(R.id.btn_two);
@@ -46,10 +54,11 @@ public class MainActivity extends AppCompatActivity {
         btnZero = findViewById(R.id.btn_zero);
         txtExpression = findViewById(R.id.txt_expression);
         txtResult = findViewById(R.id.txt_result);
+        expressionList = new ArrayList<String>();
     }
 
-
-    public void numberClick(View v) {
+    // 클리어, 지우기, 결과 버튼을 제외한 이벤트 처리 함수
+    public void buttonClick(View v) {
         switch (v.getId()) {
             case R.id.btn_one: insertExpression("1"); break;
             case R.id.btn_two: insertExpression("2"); break;
@@ -61,21 +70,68 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_eight: insertExpression("8"); break;
             case R.id.btn_nine: insertExpression("9"); break;
             case R.id.btn_zero: insertExpression("0"); break;
+            case R.id.btn_division: insertExpression("/"); break;
+            case R.id.btn_mod: insertExpression("%"); break;
+            case R.id.btn_multi: insertExpression("X"); break;
+            case R.id.btn_plus: insertExpression("+"); break;
+            case R.id.btn_minus: insertExpression("-"); break;
         }
     }
 
-
-    public void insertExpression(String str) {
-        txtExpression.append(str + " ");
-    }
-
-    String calculator() {
-
-        return "";
-    }
-
+    // 클리어 버튼 이벤트 처리
     public void clearClick(View v) {
+        expressionList.clear();
         txtExpression.setText("");
         txtResult.setText("");
+
+        Log.e("expresstionList", String.valueOf(expressionList));
+    }
+
+    // 지우기 버튼 이벤트 처리
+    public void deleteClick(View v) {
+        if (expressionList.size() != 0) {
+            expressionList.remove(expressionList.size()-1);
+
+            Log.e("expressionList", String.valueOf(expressionList));
+            txtExpression.setText(TextUtils.join(" ", expressionList));
+            Log.e("expressionList", String.valueOf(expressionList));
+        }else {
+            txtExpression.setText("");
+        }
+    }
+
+    // 표현식 변경
+    public void insertExpression(String str) {
+        try {
+            txtExpression.append(Integer.parseInt(str)+"");
+        }catch (Exception e) {
+            // operator
+            txtExpression.append(" "+str+" ");
+            Collections.addAll(expressionList, txtExpression.getText().toString().split(" "));
+        }finally {
+            Log.e("expressionList", String.valueOf(expressionList));
+        }
+    }
+
+    // 이콜 버튼 이벤트 처리
+    public void equalClick(View v) {
+        if (txtExpression.getText().toString().length() == 0) return;
+
+        String last = txtExpression.getText().toString().split(" ")[expressionList.size()];
+        if (!last.equals("X") && !last.equals("+") && !last.equals("-") && !last.equals("%") && !last.equals("/")) {
+            expressionList.add(last);
+        }
+        Log.e("expressionList", String.valueOf(expressionList));
+
+
+    }
+
+    // 연산자 우선순위 *,/,%,+,-
+    void calculator() {
+        for (String item : expressionList) {
+            if (item.equals("X")) {
+
+            }
+        }
     }
 }
