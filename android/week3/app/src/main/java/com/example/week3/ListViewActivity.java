2e62d9c63dfc1264f0ce23f78e7a2400cfd6bdd6
bbaktaeho <lth9036@gmail.com>
2020-09-20 17:53:13 +0900

    Update: ListView

--- android/week3/app/src/main/java/com/example/week3/ListViewActivity.java
+++ android/week3/app/src/main/java/com/example/week3/ListViewActivity.java
@@ -3,13 +3,21 @@ package com.example.week3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -20,26 +28,33 @@ public class ListViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
-        TextView tvSelect = findViewById(R.id.tv_select);
-        ListView listView = findViewById(R.id.listView);
+        final TextView tvSelect = findViewById(R.id.tv_select);
+        final ListView listView = findViewById(R.id.listView);
 
-        List<String> list = new ArrayList<>();
+        final List<String> list = new ArrayList<>();
+
         list.add("사과");
         list.add("배");
         list.add("딸기");
-        list.add("감자");
+        list.add("바나나");
 
-        // ArrayAdapter 에 인자값 3개 필요
-        ArrayAdapter<String> adpater = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-        // adapter 셋팅
-        listView.setAdapter(adpater);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, list);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        listView.setAdapter(adapter);
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // getItemAtPosition
-                // setText
+                int len = listView.getCount();
+                StringBuilder selects = new StringBuilder();
+                SparseBooleanArray checked = listView.getCheckedItemPositions();
+                for (int i = 0; i < len; i++)
+                    if (checked.get(i)) selects.append(" ").append(list.get(i));
+
+                tvSelect.setText(selects);
             }
         });
     }
 }
+
--- android/week3/app/src/main/java/com/example/week3/MainActivity.java
+++ android/week3/app/src/main/java/com/example/week3/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     //    alert dialog
     // builder : 다이얼로그의 속성들을 정의, create 메서드를 통해서 alert dialog 생성
     @Override
@@ -22,28 +21,29 @@ public class MainActivity extends AppCompatActivity {
         btnQuit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // positive, negative 는 위치에 따른 것.
-                // 꼭 긍정적 부정적인 로직이 올 필요는없음
-                // Builder 는 속성 지정만 함
-                AlertDialog.Builder msgBuilder = new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("앱 끈다?")
-                        .setMessage("진짜 끈다?")
-                        .setPositiveButton("꺼라", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                finish();
-                            }
-                        })
-                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                Toast.makeText(MainActivity.this, "안 끔", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                AlertDialog msgDlg = msgBuilder.create();
-                msgDlg.show();
+                showDialog();
             }
         });
+    }
+
+    void showDialog() {
+        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("앱 끈다?")
+                .setMessage("진짜 끈다?")
+                .setPositiveButton("꺼라", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "안 끔", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog msgDlg = msgBuilder.create();
+        msgDlg.show();
     }
 
 }
