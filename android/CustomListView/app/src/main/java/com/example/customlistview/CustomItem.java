8e037a2bc4b0bbfa567aa49465d6b86ad412f4f4
bbaktaeho <lth9036@gmail.com>
2020-09-23 21:34:59 +0900

    Add: CustomListView

---
+++ android/CustomListView/app/src/main/java/com/example/customlistview/CustomItem.java
@@ -0,0 +1,12 @@
+package com.example.customlistview;
+import android.graphics.drawable.Drawable;
+
+import lombok.Data;
+
+@Data
+public class CustomItem {
+    private Drawable profileImgItem;
+    private String ChatRoomItem;
+    private String ChatContentItem;
+    private String DateItem;
+}
