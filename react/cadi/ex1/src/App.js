b97435e6b20869d36621d300ad7253a46c2c5999
bbaktaeho <lth9036@gmail.com>
2020-09-30 13:52:23 +0900

    Add: React Test

---
+++ react/cadi/ex1/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import Main from './layout/Main';
+
+function App() {
+  return <Main></Main>;
+}
+
+export default App;
