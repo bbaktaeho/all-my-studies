dd0617028eb9316b4edf9339c189a741e300a96b
bbaktaeho <lth9036@gmail.com>
2020-10-13 02:30:07 +0900

    Update: ViewHolder

---
+++ android/CADI_CustomListView/app/src/main/java/com/example/cadi_customlistview/CustomViewHolder.kt
@@ -0,0 +1,11 @@
+package com.example.cadi_customlistview
+
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import android.widget.TextView
+
+class CustomViewHolder {
+    var drawableIcon: ImageView? = null
+    var textTitle: TextView? = null
+    var textSubTitle: TextView? = null
+}
--- android/CADI_CustomListView/app/src/main/java/com/example/cadi_customlistview/ListViewAdapter.kt
+++ android/CADI_CustomListView/app/src/main/java/com/example/cadi_customlistview/ListViewAdapter.kt
@@ -15,14 +15,22 @@ class ListViewAdapter(private val items: MutableList<ListViewItem>): BaseAdapter
 
     override fun getItemId(position: Int): Long = position.toLong()
 
-    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+    override fun getView(position: Int, view: View?, parent: ViewGroup?): View? {
         var convertView = view
-        if (convertView == null) convertView = LayoutInflater.from(parent?.context).inflate(R.layout.custom_list_item, parent, false)
-
+        var viewHolder: CustomViewHolder
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent?.context).inflate(R.layout.custom_list_item, parent, false)
+            viewHolder = CustomViewHolder()
+            viewHolder.drawableIcon = convertView.image_title
+            viewHolder.textTitle = convertView.text_title
+            viewHolder.textSubTitle = convertView.text_sub_title
+        }else {
+            viewHolder = convertView.tag as CustomViewHolder
+        }
         val item: ListViewItem = items[position]
-        convertView!!.image_title.setImageDrawable(item.icon)
-        convertView.text_title.text = item.title
-        convertView.text_sub_title.text = item.subTitle
+        viewHolder.drawableIcon?.image_title?.setImageDrawable(item.icon)
+        viewHolder.textTitle?.text_title!!.text = item.title
+        viewHolder.textSubTitle?.text_sub_title!!.text = item.subTitle
 
         return convertView
     }
