93ebbdcc3a1f522356122aa0f5af91e83534d3ab
bbaktaeho <lth9036@gmail.com>
2020-10-15 13:43:28 +0900

    Add: MaskInfo with Kotlin

--- android/MaskInfoKt/app/src/main/java/kr/study/maskinfokt/MainActivity.kt
+++ android/MaskInfoKt/app/src/main/java/kr/study/maskinfokt/MainActivity.kt
@@ -2,13 +2,16 @@ package kr.study.maskinfokt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.study.maskinfokt.models.Store
 
 class MainActivity : AppCompatActivity() {
+    // ktx 코틀린 확장 함수
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +25,16 @@ class MainActivity : AppCompatActivity() {
             // applicationContext, this@MainActivity
             layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
             adapter = storeAdapter
+        }
+
+        viewModel.apply {
+            itemLiveData.observe(this@MainActivity, Observer {
+                storeAdapter.updateItems(it)
+            })
+
+            loadingLiveData.observe(this@MainActivity, Observer {
+                isLoading -> progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+            })
         }
     }
 }
--- android/MaskInfoKt/app/src/main/java/kr/study/maskinfokt/MainViewModel.kt
+++ android/MaskInfoKt/app/src/main/java/kr/study/maskinfokt/MainViewModel.kt
@@ -2,12 +2,14 @@ package kr.study.maskinfokt
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import kr.study.maskinfokt.models.Store
 import kr.study.maskinfokt.repository.MaskService
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
     val itemLiveData = MutableLiveData<List<Store>>()
     val loadingLiveData = MutableLiveData<Boolean>()
 
@@ -20,5 +22,20 @@ class MainViewModel: ViewModel() {
             .build();
 
         service = retrofit.create(MaskService::class.java)
+
+        fetchStoreInfo()
+    }
+
+    //    suspend 메서드 안에서 suspend를 사용할 수 있음
+    fun fetchStoreInfo() {
+        loadingLiveData.value = true
+
+        // 코루틴에서 사용, 코드를 기다려줌
+        viewModelScope.launch {
+            val storeInfo = service.fetchStoreInfo(37.188078, 127.043002)
+            itemLiveData.value = storeInfo.stores
+
+            loadingLiveData.value = false
+        }
     }
 }
