bd71c6b61e731d4292c5205cbf6df6b036785c28
bbaktaeho <lth9036@gmail.com>
2020-10-24 14:14:40 +0900

    Complete: Prgrammers/level_1

--- javascript/Re_FP/generator2.js
+++ javascript/Re_FP/generator2.js
@@ -3,27 +3,36 @@
  */
 // 이터러블 제한
 function* gLimit(limit, iter) {
-    for (const item of iter) {
-        yield item;
-        if (item == limit) return;
-    }
+  for (const item of iter) {
+    yield item;
+    if (item == limit) return;
+  }
 }
 // 무한 수열 발생기
-function* infinity(i=0) {
-    while (true) yield i++;
+function* infinity(i = 0) {
+  while (true) yield i++;
 }
 // 홀수 발생기
 function* odds(limit) {
-    for (const item of gLimit(limit, infinity(1))) if(item % 2) yield item
+  for (const item of gLimit(limit, infinity(1))) if (item % 2) yield item;
 }
 
 var iter = odds(10);
 for (const item of iter) console.log(item);
 
 // 다양한 사용법
-console.clear()
+console.clear();
 console.log(...odds(10));
 console.log([...odds(10)]);
 var [head, ...tail] = odds(5);
-console.log("head:",head);
-console.log("tail:",tail);
+console.log('head:', head);
+console.log('tail:', tail);
+
+function* infinity() {
+  let i = 0;
+  while (true) yield ++i;
+}
+const iter = infinity();
+console.log(iter.next().value); // 1
+console.log(iter.next().value); // 2
+console.log(iter.next().value); // 3
