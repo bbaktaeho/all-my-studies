4ffc9782c8b964decf857cc561a28c4f913f7075
bbaktaeho <lth9036@gmail.com>
2021-03-12 00:07:13 +0900

    Add: 2021-03-11

--- codingTest/baekjoon/python/2839.py
+++ codingTest/baekjoon/python/2839.py
@@ -1,12 +1,11 @@
 N = int(input())
-count = 0
-while True:
-    if N % 5 == 0: 
-        count += N // 5
-        print(count)
-        break
-    N -= 3
-    count += 1
-    if N < 0:
-        print(-1)
-        break
+mod = N % 5
+if mod == 0: print(N // 5)
+elif mod == 4:
+    if N - 9 < 0: print(-1)
+    else: print((N-9) // 5 + 3)
+elif mod == 3: print(N // 5 + 1)
+elif mod == 2:
+    if N == 7: print(-1)
+    else: print((N - 12) // 5 + 4)
+elif mod == 1: print((N - 6) // 5 + 2)
